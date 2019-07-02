c6499f2100cf347302611bed4c0a4798a58973a4
Erik Madsen <ekmadsen@comcast.net>
2019-07-01 22:14:56 -0500

    Added generic DeepCopy extension method to System.Object.

--- Utilities/ExtensionMethods.cs
+++ Utilities/ExtensionMethods.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using JetBrains.Annotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 
 namespace ErikTheCoder.Utilities
@@ -18,5 +22,65 @@ namespace ErikTheCoder.Utilities
 
         [UsedImplicitly]
         public static string RemoveControlCharacters(this string Text) => Regex.Replace(Text, @"[\p{C}-[\r\n\t]]+", string.Empty);
+
+
+        // Performs a recursive copy of all class fields (public and private), except those the caller specifies should not be copied.
+        // Supports circular references and fields whose type is an interface or abstract class.
+        // In comparison, Object.MemberwiseClone in the Base Class Library performs a shallow copy.  Reference type fields of the copy point to the original reference.
+        // In other words, for reference type fields...
+        // For MemberwiseClone():  Object.ReferenceEquals(copy.FieldName, original.FieldName) == true;
+		// For DeepCopy():         Object.ReferenceEquals(copy.FieldName, original.FieldName) == false;
+		[UsedImplicitly]
+        public static T DeepCopy<T>(this T Original, List<string> SkipFields = null) where T : class
+        {
+            if (Original != null)
+            {
+                JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings
+                {
+                    // Preserving object references enables serialization to function correctly when an object graph contains circular references.
+                    //   Such as a parent object referring to a child object, and the child object referring back to the parent object.
+                    PreserveReferencesHandling = PreserveReferencesHandling.All,
+                    // Including type names increases size of serialized JSON but prevents an exception:
+                    //   "Could not create an instance of type [name].  Type is an interface or abstract class and cannot be instantiated."
+                    TypeNameHandling = TypeNameHandling.All,
+                    ContractResolver = new FieldContractResolver(SkipFields ?? new List<string>())
+                };
+                string json = JsonConvert.SerializeObject(Original, jsonSerializerSettings);
+                if (json != null) { return JsonConvert.DeserializeObject<T>(json, jsonSerializerSettings); }
+            }
+            return null;
+        }
+
+
+        // By default, Json.NET serializes public properties.
+        // This works fine for Data Transfer Objects (DTOs) that contain auto-implemented properties with no logic in getters and setters.
+        // It can cause null reference exceptions in domain objects that do contain logic in property getters and setters (if they're sensitive to the sequence in which they're called).
+        // So, copy fields instead of properties.
+        // See https://stackoverflow.com/a/24107081/8992299
+        private class FieldContractResolver : DefaultContractResolver
+        {
+            private readonly HashSet<string> _skipFields;
+
+
+            public FieldContractResolver(IEnumerable<string> SkipFields)
+            {
+                _skipFields = new HashSet<string>(SkipFields, StringComparer.CurrentCultureIgnoreCase);
+            }
+
+
+            protected override IList<JsonProperty> CreateProperties(Type Type, MemberSerialization MemberSerialization)
+            {
+                List<JsonProperty> jsonProperties = new List<JsonProperty>();
+                FieldInfo[] fields = Type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                foreach (FieldInfo field in fields)
+                {
+                    if (_skipFields.Contains(field.Name)) { continue; }
+                    JsonProperty jsonProperty = base.CreateProperty(field, MemberSerialization);
+                    jsonProperty.Readable = true;
+                    jsonProperty.Writable = true;
+                }
+                return jsonProperties;
+            }
+        }
     }
 }
